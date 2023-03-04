68ea392cb97fa9540afb5ffaad9b5b82be632cce
vennyvirtuoso <vijendra9753@gmail.com>
2023-03-05 01:46:31 +0530

    Initial Commit

---
+++ Prasang/PrasangApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrasangApp.swift
+//  Prasang
+//
+//  Created by Vijendra Vaishya on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PrasangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
